3dc893ab8147b8a56ab1a5a8b6e5edffbe4f35cf
junsak77 <jun.sakuma.bach@gmail.com>
2021-05-14 20:54:42 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,72 @@
+<?php
+
+// 数字を数え上げる
+function count_up() {
+  $limit = isset($_GET['n']) ? $_GET['n'] : 40;
+  for($i = 1; $i <= $limit; $i++) {
+    if(is_fool($i)) {
+      echo "<li>" . foolize($i) . "www (" . $i . ")" . "</li>";
+    } else {
+      echo "<li>" . $i . "</li>";
+    }
+  }
+}
+
+// アホになるかどうかを判定(3の倍数 or 3がつく)
+function is_fool($i) {
+  return ($i % 3 == 0) || (strpos($i, '3') !== false);
+}
+
+// 数字をアホにする(4桁まで対応)
+function foolize($i) {
+  $foolish = [
+    0 => "",
+    1 => "ｲﾁ",
+    2 => "ﾆ",
+    3 => "ｻﾝ",
+    4 => "ﾖﾝ",
+    5 => "ｺﾞ",
+    6 => "ﾛｸ",
+    7 => "ﾅﾅ",
+    8 => "ﾊﾁ",
+    9 => "ｷｭｰ"
+  ];
+
+  $order_name = [
+    4 => "ｾﾝ",
+    3 => "ﾋｬｸ",
+    2 => "ｼﾞｭｰ",
+    1 => ""
+  ];
+
+  $digits = str_split($i % 10000);
+  $order = strlen($i);
+  $ret = "";
+
+  foreach($digits as $digit) {
+    $add = "";
+    if($digit == 3 && $order == 4) {
+      $add = "ｻﾝｾﾞﾝ";
+    } elseif($digit == 8 && $order == 4) {
+      $add = "ﾊｯｾﾝ";
+    } elseif($digit == 3 && $order == 3) {
+      $add = "ｻﾝﾋﾞｬｸ";
+    } elseif($digit == 6 && $order == 3) {
+      $add = "ﾛｯﾋﾟｬｸ";
+    } elseif($digit == 8 && $order == 3) {
+      $add = "ﾊｯﾋﾟｬｸ";
+    } elseif($digit == 1 && $order != 1) {
+      $add = $order_name[$order];
+    } else {
+      $add = $foolish[$digit] . $order_name[$order];
+    }
+    $ret .= $add;
+    $order -= 1;
+  }
+  return $ret;
+}
+?>
+<ul>
+  <?php count_up(); ?>
+</ul>
+<div id="bottom">ｵﾓﾛｰ!!</div>
